2ef4c0eb58f52d47a6a564ec53bcc97541751644
misha1737 <mikekozlov2@gmail.com>
2020-09-12 23:23:36 +0300

    refactoring postEdit

--- src/store/posts.js
+++ src/store/posts.js
@@ -3,50 +3,46 @@ import Post from './post_help.js'
 export default {
 
     state: {
-        newPost: {
-            postName: null,
-            postContent: null,
-            description: null,
-            userId: null,
-            id: null
-        },
-        timeInLastPost:null,
-        posts:[]
+        selectedPost: {},
+        timeInLastPost: null,
+        posts: [],
+        uploadImgValue: null,
     },
     mutations: {
-        loadPosts(state, payload){
-            
-            payload.forEach(key=>{
+        loadPosts(state, payload) {
+            payload.forEach(key => {
                 state.posts.push(key)
             });
-            state.timeInLastPost = payload[payload.length-1].time;
+            state.timeInLastPost = payload[payload.length - 1].time;
             state.posts.pop();
         },
-        savePost(state, payload) {
-            state.newPost = payload;
-        },
-        getPosts(state, payload){
+        getPosts(state, payload) {
             state.posts = payload;
         },
-       
-        
+        updateImg(state, payload) {
+            state.selectedPost.url = payload;
+        },
+        uploadImgValue(state, payload){
+            state.uploadImgValue=payload;
+        }
+
     },
     actions: {
-      
-        async getPosts({commit,getters}) {
+
+        async getPosts({ commit, getters }) {
             commit('clearError')
             commit('setLoading', true)
             try {
-             let post;
-             if (!getters.timeInLastPost)
-            post= await firebase.database().ref('posts').orderByChild('time').startAt(0).limitToLast(11).once('value')
-             else
-            post= await firebase.database().ref('posts').orderByChild('time').endAt(getters.timeInLastPost).limitToLast(11).once('value')
+                let post;
+                if (!getters.timeInLastPost)
+                    post = await firebase.database().ref('posts').orderByChild('time').startAt(0).limitToLast(11).once('value')
+                else
+                    post = await firebase.database().ref('posts').orderByChild('time').endAt(getters.timeInLastPost).limitToLast(11).once('value')
 
-                const posts =  post.val();
+                const posts = post.val();
                 let postsArray = [];
 
-                Object.keys(posts).forEach(key=>{
+                Object.keys(posts).forEach(key => {
                     const p = posts[key]
                     postsArray.push(
                         new Post(
@@ -60,10 +56,10 @@ export default {
                             p.publish,
                             p.time
                         )
-                    ) 
+                    )
                 })
-                
-                postsArray= postsArray.sort((a, b) => a.time < b.time ? 1 : -1)
+
+                postsArray = postsArray.sort((a, b) => a.time < b.time ? 1 : -1)
                 commit('getPosts', postsArray)
                 commit('setLoading', false)
             } catch (error) {
@@ -73,20 +69,20 @@ export default {
             }
         },
 
-        async loadPosts({commit,getters}) {
+        async loadPosts({ commit, getters }) {
             commit('clearError')
             commit('setLoading', true)
             try {
-             let post;
-             if (!getters.timeInLastPost)
-            post= await firebase.database().ref('posts').orderByChild('time').startAt(0).limitToLast(11).once('value')
-             else
-            post= await firebase.database().ref('posts').orderByChild('time').endAt(getters.timeInLastPost).limitToLast(11).once('value')
+                let post;
+                if (!getters.timeInLastPost)
+                    post = await firebase.database().ref('posts').orderByChild('time').startAt(0).limitToLast(11).once('value')
+                else
+                    post = await firebase.database().ref('posts').orderByChild('time').endAt(getters.timeInLastPost).limitToLast(11).once('value')
 
-                const posts =  post.val();
+                const posts = post.val();
                 let postsArray = [];
 
-                Object.keys(posts).forEach(key=>{
+                Object.keys(posts).forEach(key => {
                     const p = posts[key]
                     postsArray.push(
                         new Post(
@@ -100,10 +96,10 @@ export default {
                             p.publish,
                             p.time
                         )
-                    ) 
+                    )
                 })
-                
-                postsArray= postsArray.sort((a, b) => a.time < b.time ? 1 : -1)
+
+                postsArray = postsArray.sort((a, b) => a.time < b.time ? 1 : -1)
                 commit('loadPosts', postsArray)
                 commit('setLoading', false)
             } catch (error) {
@@ -119,7 +115,7 @@ export default {
             commit('setLoading', true)
             try {
                 //logic
-                
+
                 const newPost = new Post(
                     payload.postName,
                     payload.postContent,
@@ -130,31 +126,70 @@ export default {
                     payload.category,
                     payload.publish,
                     firebase.database.ServerValue.TIMESTAMP
-                    );
+                );
                 const post = await firebase.database().ref('posts').push(newPost);
                 
                 commit('savePost', newPost);
-                commit('setLoading', false)
+                commit('setLoading', false);
+                return post.key;
             } catch (error) {
                 commit('setLoading', false)
                 commit('setError', error.message)
                 throw error
             }
         },
-        async removePost ({ commit, getters }, payload) {
+        async uploadImg({ commit, getters }, payload) {
+            commit('clearError')
+            commit('setLoading', true)
+            try {
+                //видалення старої картинки
+                firebase.storage().ref("posts/").child(getters.selectedPost.id).listAll()
+                    .then(function (result) {
+                        result.items.forEach((item) => {
+                            firebase.storage().ref(item.location.path_).delete();
+                        });
+                    });
+                    commit('updateImg', null);
+                const storageRef = firebase.storage().ref(`posts/${getters.selectedPost.id}/${payload.name}`).put(payload);
+                storageRef.on("state_changed",
+                    (snapshot) => {
+                        let uploadValue=0;
+                        uploadValue=((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                        commit('uploadImgValue', uploadValue);
+                    },
+                    (error) => {
+                        console.log(error.message);
+                    },
+                    () => {
+                        storageRef.snapshot.ref.getDownloadURL().then((urlImg) => {
+                            commit('updateImg', urlImg);
+                            firebase.database().ref('posts/' + getters.selectedPost.id).update(
+                                {url:urlImg}
+                            );
+                            commit('uploadImgValue', null);
+                        });
+                    }
+                );
+                commit('setLoading', false)
+            } catch (error) {
+                commit('setLoading', false)
+                commit('setError', error.message)
+                throw error
+            }
+
+        },
+        async removePost({ commit, getters }, payload) {
             commit('clearError')
             commit('setLoading', true)
             try {
                 //logic
-                await firebase.database().ref('posts/'+payload).remove();
+                await firebase.database().ref('posts/' + payload).remove();
                 commit('setLoading', false);
             } catch (error) {
                 commit('setLoading', false);
                 commit('setError', error.message);
                 throw error
             }
-
-            
         },
 
         async updatePost({ commit, getters }, payload) {
@@ -172,8 +207,8 @@ export default {
                     payload.category,
                     payload.publish,
                     firebase.database.ServerValue.TIMESTAMP
-                    );
-                 await firebase.database().ref('posts/'+payload.id).update(
+                );
+                await firebase.database().ref('posts/' + payload.id).update(
                     updatedPost
                 );
                 commit('setLoading', false)
@@ -184,25 +219,33 @@ export default {
             }
         },
 
-            
+
 
 
     },
 
     getters: {
-        AllPosts: state=>{
+        AllPosts: state => {
             return state.posts
         },
-        posts: state=>filter=>{
-            return state.posts.filter(item=>{
-               return item.category === filter
+        selectedPost: state => {
+            return state.selectedPost
+        },
+        posts: state => filter => {
+            return state.posts.filter(item => {
+                return item.category === filter
             })
         },
-        post: state=>id=>{
-            return state.posts.find(state => state.id === id);
+        post: state => id => {
+            state.selectedPost = state.posts.find(state => state.id === id);
+            return state.selectedPost
         },
-        timeInLastPost:state=>{
+        timeInLastPost: state => {
             return state.timeInLastPost
         },
+        uploadImgValue: state => {
+            return state.uploadImgValue
+        },
+        
     }
 }
--- src/views/PostEdit.vue
+++ src/views/PostEdit.vue
@@ -8,176 +8,106 @@
     </span>
     <h1 v-if="id==0">Create Post</h1>
     <h1 v-else>Post edit</h1>
-    <label for="postName">Post Name</label>
-    <input v-model="postName" id="postName" type="text" placeholder="Enter PostName" />
-    <label for="description">Description</label>
-    <textarea v-model="description" id="description" placeholder="Enter description"></textarea>
-    <label for="postContent">Post Content</label>
-    <vue-editor v-model="postContent" />
-    <div :class="{disabled : id==0}" class="uploadImage">
-      <div v-if="progress">
-        <p>
-          Progress: {{uploadValue.toFixed()+"%"}}
-          <progress :value="uploadValue" max="100"></progress>
-        </p>
-      </div>
-      <div>
-        <img class="preview" :src="picture" />
-      </div>
-      <input type="file" ref="inputFile" class="inputFile" @change="previewImage" accept="image/" />
-      <button class="button" @click="OpenFileSelect()">Upload picture</button>
-    </div>
-    <div class="choice">
-      <input
-        v-model="publish"
-        type="radio"
-        id="contactChoice1"
-        name="contact"
-        :value="false"
-        checked
-      />
-      <label for="contactChoice1">Draft</label>
-      <input v-model="publish" type="radio" id="contactChoice2" name="contact" :value="true" />
-      <label for="contactChoice2">Publish</label>
-    </div>
-    <div class="select">
-    <select v-model="category" size="1" name="hero[]">
-      <option :value="null" selected disabled>Select category</option>
-      <option
-        :value="category.id"
-        v-for="category in categories"
-        :key="category.id"
-      >{{category.categoryName}}</option>
-    </select>
-    </div>
-    <button v-if="!loading" class="button" @click="submit()">save</button>
-    <button v-else class="button disabled">Loading...</button>
+    <div v-if="post">
+      <label for="postName">Post Name</label>
+      <input v-model="post.postName" id="postName" type="text" placeholder="Enter PostName" />
+      <label for="description">Description</label>
+      <textarea v-model="post.description" id="description" placeholder="Enter description"></textarea>
+      <label for="postContent">Post Content</label>
+      <vue-editor v-model="post.postContent" />
 
+      <div :class="{disabled : id==0}" class="uploadImage">
+        <div v-if="uploadImgValue">
+          <p>
+            Progress: {{uploadImgValue.toFixed()+"%"}}
+            <progress :value="uploadImgValue" max="100"></progress>
+          </p>
+        </div>
+        <div>
+          <img class="preview" :src="post.url" />
+        </div>
+        <input type="file" ref="inputFile" class="inputFile" @change="previewImage" accept="image/" />
+        <button class="button" @click="OpenFileSelect()">Upload picture</button>
+      </div>
+
+      <div class="choice">
+        <input
+          v-model="post.publish"
+          type="radio"
+          id="contactChoice1"
+          name="contact"
+          :value="false"
+          checked
+        />
+        <label for="contactChoice1">Draft</label>
+        <input v-model="post.publish" type="radio" id="contactChoice2" name="contact" :value="true" />
+        <label for="contactChoice2">Publish</label>
+      </div>
+      <div class="select" v-if="categories">
+        <select v-model="post.category" size="1" name="category[]">
+          <option :value="null" selected disabled>Select category</option>
+          <option
+            :value="category.id"
+            v-for="category in categories"
+            :key="category.id"
+          >{{category.categoryName}}</option>
+        </select>
+      </div>
+    </div>
+    <button v-if="!loading" class="button" @click="save()">save</button>
+    <button v-else class="button disabled">Loading...</button>
     <span class="errorMsg">{{submitStatus}}</span>
   </div>
 </template>
-
 <script>
-import firestore from "firebase/app";
-import { VueEditor } from "vue2-editor";
 import modal from "./../components/Modal.vue";
+import { VueEditor } from "vue2-editor";
 export default {
   name: "MainContent",
-
   data() {
     return {
-      postName: "",
-      postContent: "",
-      description: "",
-      submitStatus: null,
       modalOpen: false,
-      //upl img
-      imageData: null,
-      picture: null,
-      uploadValue: 0,
-      oldImgUrl: "",
-      category: null,
-      publish: false,
-      inputFile: this.$refs.inputFile,
-      progress: false,
+      submitStatus: null,
     };
   },
   props: {
     id: String,
   },
   components: { VueEditor, modal },
-  validations: {},
-  methods: {
-    removePost(result) {
-      if (!result) {
-        this.modalOpen = false;
-        return;
-      }
-      this.$store
-        .dispatch("removePost", this.id)
-        .then(() => {
-          this.submitStatus = "OK";
-          this.$store.dispatch("getPosts");
-          this.$router.push("/");
-        })
-        .catch((err) => {
-          console.log(err);
-          this.submitStatus = err;
-        });
+  computed: {
+    loading() {
+      return this.$store.getters.loading;
     },
-    previewImage(event) {
-      this.progress = true;
-      this.uploadValue = 0;
-      this.oldImgUrl = this.picture;
-      this.picture = null;
-      this.imageData = event.target.files[0];
-      this.uploadImg();
+    categories() {
+      return this.$store.getters.categories;
     },
-    OpenFileSelect() {
-       if (this.id != 0) {
-      this.inputFile.click();
-       }
+    uploadImgValue() {
+      return this.$store.getters.uploadImgValue;
     },
-    uploadImg() {
+    post() {
       if (this.id == 0) {
-        return;
+        return {
+          postName: "",
+          postContent: "",
+          description: "",
+          id: null,
+          url: null,
+          category: null,
+          publish: false,
+        };
       }
-
-      if (this.picture != this.oldImgUrl) {
-        firestore
-          .storage()
-          .ref("posts/")
-          .child(`${this.id}`)
-          .listAll()
-          .then(function (result) {
-            result.items.forEach((item) => {
-              firestore.storage().ref(item.location.path_).delete();
-            });
-          });
-      }
-
-      this.picture = null;
-      const storageRef = firestore
-        .storage()
-        .ref(`posts/${this.id}/${this.imageData.name}`)
-        .put(this.imageData);
-      storageRef.on(
-        "state_changed",
-        (snapshot) => {
-          this.uploadValue =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        },
-        (error) => {
-          console.log(error.message);
-        },
-        () => {
-          this.uploadValue = 100;
-          storageRef.snapshot.ref.getDownloadURL().then((url) => {
-            this.picture = url;
-            this.progress = false;
-          });
-        }
-      );
+      return this.$store.getters.post(this.id);
     },
-    submit() {
-      const postObj = {
-        postName: this.postName,
-        postContent: this.postContent,
-        description: this.description,
-        id: this.id == 0 ? null : this.id,
-        url: this.picture ? this.picture : null,
-        category: this.category,
-        publish: this.publish,
-      };
-      //delete old img
-
-      //for create post
+  },
+  methods: {
+    save() {
       if (this.id == 0) {
         this.$store
-          .dispatch("savePost", postObj)
-          .then(() => {
+          .dispatch("savePost", this.post)
+          .then((id) => {
             this.submitStatus = "OK";
             this.$store.dispatch("getPosts");
+            this.$router.push("/postEdit/" + id);
           })
           .catch((err) => {
             console.log(err);
@@ -186,7 +116,7 @@ export default {
       } //for edit post
       else {
         this.$store
-          .dispatch("updatePost", postObj)
+          .dispatch("updatePost", this.post)
           .then(() => {
             this.submitStatus = "OK";
             this.$store.dispatch("getPosts");
@@ -197,70 +127,27 @@ export default {
           });
       }
     },
-  },
-  mounted() {
-    this.inputFile = this.$refs.inputFile;
-    if (this.post && this.id != 0) {
-      this.postName = this.post.postName;
-      this.postContent = this.post.postContent;
-      this.description = this.post.description;
-      this.picture = this.post.url;
-      this.category = this.post.category;
-      this.publish = this.post.publish;
-    } else {
-      this.postName = "";
-      this.postContent = "";
-      this.description = "";
-      this.picture = "";
-      this.category = null;
-      this.publish = false;
-    }
-  },
-  computed: {
-    loading() {
-      return this.$store.getters.loading;
-    },
-    post() {
-      return this.$store.getters.post(this.id);
-    },
-    categories() {
-      return this.$store.getters.categories;
-    },
-  },
-  watch: {
-    id: function (val) {
-      if (this.post && this.id != 0) {
-        this.postName = this.post.postName;
-        this.postContent = this.post.postContent;
-        this.description = this.post.description;
-        this.picture = this.post.url;
-        this.category = this.post.category;
-        this.publish = this.post.publish;
-      } else {
-        this.postName = "";
-        this.postContent = "";
-        this.description = "";
-        this.picture = "";
-        this.category = null;
-        this.publish = false;
+
+    OpenFileSelect() {
+      if (this.id != 0) {
+        this.$refs.inputFile.click();
       }
     },
-    post: function (val) {
-      if (this.post && this.id != 0) {
-        this.postName = this.post.postName;
-        this.postContent = this.post.postContent;
-        this.description = this.post.description;
-        this.picture = this.post.url;
-        this.category = this.post.category;
-        this.publish = this.post.publish;
-      } else {
-        this.postName = "";
-        this.postContent = "";
-        this.description = "";
-        this.picture = "";
-        this.category = null;
-        this.publish = false;
-      }
+    previewImage(event) {
+      this.oldImgUrl = this.post.url;
+      this.post.url = null;
+      this.imageData = event.target.files[0];
+      this.post.url = window.URL.createObjectURL(this.imageData);
+
+      this.$store
+        .dispatch("uploadImg", this.imageData)
+        .then(() => {
+          this.submitStatus = "OK";
+        })
+        .catch((err) => {
+          console.log(err);
+          this.submitStatus = err;
+        });
     },
   },
 };
@@ -270,4 +157,3 @@ export default {
 <style lang="scss">
 @import "./../scss/components/_editText.scss";
 </style>
- 
