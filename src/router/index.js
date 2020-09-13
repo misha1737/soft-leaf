6fd04f599ca6db58130867b865844f27283887ea
misha1737 <mikekozlov2@gmail.com>
2020-09-13 10:29:28 +0300

    fix scrool bag

--- src/router/index.js
+++ src/router/index.js
@@ -84,7 +84,11 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    // возвращаем требуемую позицию прокрутки
+    return { x: 0, y: 0 }
+  }
 })
 
 export default router
